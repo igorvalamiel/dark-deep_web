67950840f864504c6c6d545eb1f9dbbc563e4e46
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-04 12:22:38 -0300

    update

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const button = document.getElementById("hackButton");
 const overlay = document.getElementById("overlay");
-const content = document.getElementById("content");
 
 button.addEventListener("click", () => {
     overlay.style.display = "flex";
     overlay.classList.add("flash");
 
     setTimeout(() => {
-        overlay.classList.remove("flash");
-        overlay.innerHTML = "<h1>ACCESS GRANTED</h1>";
-    }, 3000);
+        window.location.href = "deep.html";
+    }, 2000);
 });
