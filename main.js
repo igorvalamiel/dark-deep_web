1a1806c34306e7b769332525fc7a1a7d2d1fb12c
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-04 12:16:43 -0300

    testando ideias

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-window.onload = function () {
-    const overlay = document.getElementById("overlay");
-    const content = document.getElementById("content");
+const button = document.getElementById("hackButton");
+const overlay = document.getElementById("overlay");
+const content = document.getElementById("content");
 
+button.addEventListener("click", () => {
+    overlay.style.display = "flex";
     overlay.classList.add("flash");
 
     setTimeout(() => {
-        overlay.style.display = "none";
-        document.body.style.overflow = "auto";
-        content.style.visibility = "visible";
+        overlay.classList.remove("flash");
+        overlay.innerHTML = "<h1>ACCESS GRANTED</h1>";
     }, 3000);
-};
+});
